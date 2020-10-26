d490e0971502188e43fe1aedada6887e8436c038
unknown <reproduct1on@gmail.com>
2020-10-26 18:36:46 +0300

    lesson4

---
+++ search/crawler/pkg/spider/local.go
@@ -0,0 +1,16 @@
+// Package spider реализует сканер содержимого веб-сайтов.
+// Пакет позволяет получить список ссылок и заголовков страниц внутри веб-сайта по его URL.
+package spider
+
+// LocScan Заглушка для тестов.
+type LocScan struct {
+	Interface
+}
+
+// Scan возвращает ассоциативный массив: ссылок-описание
+func (l *LocScan) Scan(depth int) (data map[string]string, err error) {
+	data = map[string]string{
+		"https://www.mail.ru": "Mail.ru: почта, поиск в интернете, новости, игры",
+	}
+	return data, nil
+}
---
+++ search/go.mod
@@ -0,0 +1,5 @@
+module search
+
+go 1.15
+
+require golang.org/x/net v0.0.0-20201026091529-146b70c837a4
